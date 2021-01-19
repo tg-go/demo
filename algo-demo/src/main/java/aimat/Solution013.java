63210ae1853e3567a531e61de46b42b7ef16a225
bo.luo <bo.luo@qunar.com>
2021-01-19 19:30:52 +0800

    sort

--- algo-demo/src/main/java/aimat/Solution013.java
+++ algo-demo/src/main/java/aimat/Solution013.java
@@ -8,6 +8,12 @@ public class Solution013 {
 
 
     public int movingCount(int m, int n, int k) {
-        return 1;
+        int[] dp = new int[n+1];
+        for(int i = 2;i<=n;i++){
+            for(int j = 1; j < i;j++){
+                dp[i] = Math.max(dp[i],Math.max(j*(i-j),j*dp[i-j]));
+            }
+        }
+        return dp[n];
     }
 }
