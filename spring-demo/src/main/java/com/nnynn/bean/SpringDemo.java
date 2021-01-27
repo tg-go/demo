73e0e042f4a15628ba79a4d293af77989c80a879
bo.luo <bo.luo@qunar.com>
2021-01-27 23:51:47 +0800

    leetcode

--- spring-demo/src/main/java/com/nnynn/bean/SpringDemo.java
+++ spring-demo/src/main/java/com/nnynn/bean/SpringDemo.java
@@ -1,7 +1,9 @@
 package com.nnynn.bean;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
 /**
  * @author bo.luo
@@ -12,6 +14,8 @@ public class SpringDemo {
     public static void main(String[] args) {
         XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring.xml"));
         Student student= beanFactory.getBean(Student.class);
+        ApplicationContext applicationContext = new XmlWebApplicationContext();
+        applicationContext.getBean("aa");
         System.out.println(student);
         beanFactory.destroyBean(student);
     }
